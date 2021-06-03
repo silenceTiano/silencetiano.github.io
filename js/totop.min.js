37cf2aedbdffa6ebe60fb23517c57ebe2f48c193
silence <silence_hgt@163.com>
2021-06-03 11:21:04 +0800

    Site updated: 2021-06-03 11:20:53

---
+++ js/totop.min.js
@@ -0,0 +1,1 @@
+$(window).scroll(function(){$(window).scrollTop()>$(window).height()*.5?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},1e3,function(){$("#rocket").removeClass("show launch")});return false});
